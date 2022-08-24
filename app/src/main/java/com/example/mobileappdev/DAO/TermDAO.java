a29279edabb419347c3f466fcb8a6be6dfc0e97e
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-08-24 12:40:07 -0700

    Added DAO

---
+++ app/src/main/java/com/example/mobileappdev/DAO/TermDAO.java
@@ -0,0 +1,25 @@
+package com.example.mobileappdev.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
+import com.example.mobileappdev.Entity.Term;
+
+@Dao
+public interface TermDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Term term);
+
+    @Update
+    void update(Term term);
+
+    @Delete
+    void delete(Term term);
+
+    @Query("SELECT FROM courses ORDER BY courseID ASC")
+    List<>
+
+}
---
+++ app/src/main/java/com/example/mobileappdev/UI/CourseList.java
@@ -0,0 +1,4 @@
+package com.example.mobileappdev.UI;
+
+public class CourseList {
+}
